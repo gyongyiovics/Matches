38057d3415c550188b64949599c64baa101c7dd7
gyongyiovics <gyongyimako@gmail.com>
2022-01-24 10:10:07 +0100

    added pullMatchFromBox to Computer.java, refactored pullMatchFromBox() in Game.java

--- src/main/java/gameField/Game.java
+++ src/main/java/gameField/Game.java
@@ -7,17 +7,19 @@ import players.Player;
 public class Game {
     Player player1 = null;
     Player player2 = null;
-
     MatchBox matchBox;
 
     public void pullMatchFromBox() {
         player1 = new Player("Gyufás Rufusz");
         player2 = new Computer("Mikrobi");
         matchBox = new MatchBox();
+        int matches = matchBox.matchInBox();
         //this is the amout of the matches players have in the box
-        System.out.println("Pull some matches between 0 and " + matchBox.getAmount());
+        System.out.println("Pull some matches between 0 and " + matches);
 
-        while (matchBox.getAmount() > 1) {
+        //TODO: while (true)
+        //TODO: do not make in infinite!
+        /*while (*//*matchBox.getAmount() > 1*//*true) {
             Player player = new Player();
             Player enemy = new Player();
 
@@ -34,7 +36,31 @@ public class Game {
             if(matchBox.getAmount() == 1 | matchBox.getAmount() == 0 | matchBox.getAmount() == -1) {
                 System.out.println(enemy.getName() + " won!");
             }
-        }
+        }*/
+
+        while (matches >= 0) {
+            System.out.println("Matches on the table: " + matchBox.getAmount());
+            System.out.println("Take 1-3 matches!");
+            int playerMatches = player1.pullMatchFromBox(matchBox);
+            matches -= playerMatches;
+
+            if (matches == 1) {
+                System.out.println("You won!");
+                break;
+            }
+
+            int enemyMatches = player2.pullMatchFromBox(matchBox);
+            matches -= enemyMatches;
+
+            System.out.println("Computer took " + enemyMatches + " matches.");
+
+            if (matches == 1) {
+                System.out.println("Computer won!");
+                break;
+            }
+
+        } /*while (true);*/
+        System.out.println("remaining matches: " + matches);
     }
 
 }
--- src/main/java/players/Computer.java
+++ src/main/java/players/Computer.java
@@ -1,12 +1,26 @@
 package players;
 
-public class Computer extends Player{
+import matches.MatchBox;
 
-    public Computer(){};
+import java.util.Random;
+
+public class Computer extends Player{
+    private final int MAX = 4;
+    private final int MIN = 1;
+
+    public Computer(){}
 
     public Computer(String name) {
         super(name);
     }
 
-    //TODO: override players method - how many matches pulled from box?
+    @Override
+    public int pullMatchFromBox(MatchBox box) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((MAX - MIN) + 1) + MIN;
+        int matchInBox = box.setAmount(box.getAmount() - randomNum);
+        System.out.println(this.getName() + " pulled " + randomNum + " matches.");
+        System.out.println("The number of matches in this box: " + box.getAmount());
+        return matchInBox;
+    }
 }
